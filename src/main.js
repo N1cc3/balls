3ae8010b31c3fece8937ebe0c51a707d23284943
N1cc3 <niclaslindgren91@hotmail.com>
2017-05-31 17:25:17 +0300

    Simple scene with yellow ball. Moved some dependencies to dev.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,34 @@
-// TODO: Write code
-document.write("dem ballz");
+const THREE = require("../node_modules/three/build/three.min.js");
+
+// SCENE, CAMERA, RENDERER
+let scene = new THREE.Scene();
+let aspect = window.innerWidth / window.innerHeight;
+let camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 10000);
+camera.position.z = 5;
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.domElement.setAttribute('tabIndex', '0');
+renderer.domElement.focus();
+
+// LIGHTS
+let ambientLight = new THREE.AmbientLight(0x444444, 0.1);
+let light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 1000, 0);
+scene.add(ambientLight);
+scene.add(light);
+
+// FLOOR
+
+// GAME OBJECTS
+let geometry = new THREE.SphereGeometry( 1, 32, 32 );
+let material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+let sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+// GAME LOOP
+function render() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+render();
