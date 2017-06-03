ba0985aa3d65249ff589818cb37a292e33313069
N1cc3 <niclaslindgren91@hotmail.com>
2017-06-03 23:42:59 +0300

    Made shadow camera larger to fix clipped shadows.

--- src/main.js
+++ src/main.js
@@ -29,6 +29,10 @@ let ambientLight = new THREE.AmbientLight(0x444444, 0.5);
 let light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(25, 50, 25);
 light.castShadow = true;
+light.shadowCameraLeft = -20;
+light.shadowCameraRight = 20;
+light.shadowCameraTop = 20;
+light.shadowCameraBottom = -20;
 SCENE.add(ambientLight);
 SCENE.add(light);
 
