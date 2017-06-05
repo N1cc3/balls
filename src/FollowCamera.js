c55f737acb9334af11fc4e1ed3e3ebf6739807e6
N1cc3 <niclaslindgren91@hotmail.com>
2017-06-06 01:40:45 +0300

    Example of private function and trailing space diff mess-up.

--- src/FollowCamera.js
+++ src/FollowCamera.js
@@ -1,15 +1,15 @@
 import * as THREE from 'three';
 
 class FollowCamera extends THREE.PerspectiveCamera {
-    
+
   constructor(baseRotation = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(-1, 0, 0), Math.PI/24),
               maxRotDeviation = 30*(Math.PI/180),
               followVel = 0.01,
               angVel = 0.002,
               baseDistance = 10,
-              fov = 75, 
-              aspect = window.innerWidth / window.innerHeight, 
-              near = 0.1, 
+              fov = 75,
+              aspect = window.innerWidth / window.innerHeight,
+              near = 0.1,
               far = 10000) {
     super(fov, aspect, near, far);
 
@@ -31,7 +31,7 @@ class FollowCamera extends THREE.PerspectiveCamera {
     this.position.set(targetPos.x + posVec.x, targetPos.y + posVec.y, targetPos.z + posVec.z);
     this.lookAt(targetPos);
   }
-  
+
   update(delta, heading) {
     let targetPos = this.target.position;
     let posVec = this.calcPosVec(delta, heading);
@@ -39,15 +39,15 @@ class FollowCamera extends THREE.PerspectiveCamera {
     this.position.set(targetPos.x + posVec.x, targetPos.y + posVec.y, targetPos.z + posVec.z);
     this.lookAt(targetPos);
   }
-  
+
   calcPosVec(delta, heading) {
-    
+
     let rotation = this.calcRotation(delta, heading);
     let posVec = this.calcBackVec(delta);
 
-    return posVec.applyQuaternion(rotation);   
+    return posVec.applyQuaternion(rotation);
   }
-  
+
   calcRotation(delta, heading) {
     let goalQuat;
     if (heading.length() != 0) {
@@ -57,40 +57,40 @@ class FollowCamera extends THREE.PerspectiveCamera {
     } else {
       goalQuat = this.baseRotation.clone();
     }
-    
-    let rotDiff = this.quaternionAngleDiff(this.currRotation, goalQuat);
+
+    let rotDiff = quaternionAngleDiff(this.currRotation, goalQuat);
     let t = Math.min(1, delta*(this.angVel/rotDiff));
     return this.currRotation.slerp(goalQuat, t).clone();
   }
-  
-  quaternionAngleDiff(q1, q2) {
-    return Math.acos(q1.dot(q2))/(q1.length()*q2.length());
-  }
-  
+
   calcBackVec(delta) {
     let thispos = this.truePos.clone();
     let targetPos = this.target.position.clone();
     let posDiff = new THREE.Vector3(targetPos.x - thispos.x, targetPos.y - thispos.y, targetPos.z - thispos.z);
-    
+
     /*
     if (Math.abs(Math.atan(posDiff.y/posDiff.x)) < Math.PI/4) {
         thispos.y = 0;
     }
     */
-    
+
     if (posDiff.length() > this.baseDistance && !isNaN(delta)) {
         this.truePos.lerp(thispos.add(posDiff), delta*this.followVel*(1 - this.baseDistance/posDiff.length()));
     }
 
     return this.getRelativePos();
   }
-  
+
   getRelativePos() {
     let thisPos = this.truePos;
     let targetPos = this.target.position;
     return new THREE.Vector3(thisPos.x - targetPos.x, thisPos.y - targetPos.y, thisPos.z - targetPos.z);
   }
-  
+
+}
+
+function quaternionAngleDiff(q1, q2) {
+  return Math.acos(q1.dot(q2))/(q1.length()*q2.length());
 }
 
 export default FollowCamera;
