c889ddff544020ff3db6941f715ec0e2c244004d
N1cc3 <niclaslindgren91@hotmail.com>
2017-06-08 00:54:38 +0300

    Fixed linter errors.

--- src/main.js
+++ src/main.js
@@ -5,37 +5,36 @@ import FollowCamera from './FollowCamera';
 import Box from './objects/Box';
 import Ball from './objects/Ball';
 import Game from './Game';
-import {SCENE, PHYSICS} from './Game';
+import {SCENE,} from './Game';
 import HalfPipe from './levels/HalfPipe';
-import {MATERIALS} from './Physics';
 
 // SCENE, CAMERA, RENDERER
-let game = new Game();
+const game = new Game();
 let aspect = window.innerWidth / window.innerHeight;
-let followCamera = new FollowCamera();
+const followCamera = new FollowCamera();
 followCamera.position.z = 20;
 
-let audioLoader = new THREE.AudioLoader();
-let listener = new THREE.AudioListener();
+const audioLoader = new THREE.AudioLoader();
+const listener = new THREE.AudioListener();
 followCamera.add(listener);
 
-let renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 renderer.domElement.setAttribute('tabIndex', '0');
 renderer.domElement.focus();
 
-window.onresize = function () {
-  aspect = window.innerWidth / window.innerHeight
+window.onresize = () => {
+  aspect = window.innerWidth / window.innerHeight;
   followCamera.aspect = aspect;
   followCamera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 };
 
 // LIGHTS
-let ambientLight = new THREE.AmbientLight(0x444444, 0.5);
-let light = new THREE.DirectionalLight(0xffffff, 1);
+const ambientLight = new THREE.AmbientLight(0x444444, 0.5);
+const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(25, 50, 25);
 light.castShadow = true;
 light.shadow.camera.left = -20;
@@ -46,7 +45,7 @@ SCENE.add(ambientLight);
 SCENE.add(light);
 
 // GAME OBJECTS
-let ball = new Ball(1, 32, '#ff0000');
+const ball = new Ball(1, 32, '#ff0000');
 ball.position.set(0, 5, 10);
 
 followCamera.setTarget(ball);
@@ -54,21 +53,23 @@ followCamera.setTarget(ball);
 // PHYSICS
 game.addObject(ball);
 
-for (let i = 0; i < 10; i++) {
-  let box = new Box(1, 1, 1, '#ffff00');
+const boxSpawnAmount = 10;
+for (let i = 0; i < boxSpawnAmount; i++) {
+  const box = new Box(1, 1, 1, '#ffff00');
   box.position.set(-5 + i, 5 + 0.2 * i, 0);
   game.addObject(box);
 }
 
 // SOUNDS
-audioLoader.load('../sounds/thump.mp3', function(buffer) {
-  for (let object of game.objects) {
+audioLoader.load('../sounds/thump.mp3', (buffer) => {
+  for (const object of game.objects)
     object.onCollide((e) => {
-      let contact = e.contact;
-      let hitSpeed = Math.abs(contact.getImpactVelocityAlongNormal());
-      let volume = Math.min(1, 0.02 * hitSpeed);
-      if (volume > 0.02) {
-        let sound = new THREE.PositionalAudio(listener);
+      const contact = e.contact;
+      const hitSpeed = Math.abs(contact.getImpactVelocityAlongNormal());
+      const volumeFactor = 0.02;
+      const volume = Math.min(1, volumeFactor * hitSpeed);
+      if (volume > volumeFactor) {
+        const sound = new THREE.PositionalAudio(listener);
         sound.setBuffer(buffer);
         sound.setRefDistance(1);
         sound.setVolume(volume);
@@ -76,7 +77,6 @@ audioLoader.load('../sounds/thump.mp3', function(buffer) {
         sound.play();
       }
     });
-  }
 });
 
 game.loadLevel(new HalfPipe());
@@ -84,40 +84,40 @@ game.loadLevel(new HalfPipe());
 // GAME LOOP
 let previousTime;
 function render() {
-  let time = new Date().getTime();
-  let delta = time - previousTime;
+  const time = new Date().getTime();
+  const delta = time - previousTime;
   previousTime = time;
 
   // PLAYER CONTROLS
-  let up = keymaster.isPressed('w');
-  let down = keymaster.isPressed('s');
-  let left = keymaster.isPressed('a');
-  let right = keymaster.isPressed('d');
+  const up = keymaster.isPressed('w');
+  const down = keymaster.isPressed('s');
+  const left = keymaster.isPressed('a');
+  const right = keymaster.isPressed('d');
 
-  let forceDirection = new CANNON.Vec3(0, 0, 0);
-  if (up) {
+  const forceDirection = new CANNON.Vec3(0, 0, 0);
+  if (up)
     forceDirection.vadd(new CANNON.Vec3(0, 0, -1), forceDirection);
-  }
-  if (down) {
+
+  if (down)
     forceDirection.vadd(new CANNON.Vec3(0, 0, 1), forceDirection);
-  }
-  if (left) {
+
+  if (left)
     forceDirection.vadd(new CANNON.Vec3(-1, 0, 0), forceDirection);
-  }
-  if (right) {
+
+  if (right)
     forceDirection.vadd(new CANNON.Vec3(1, 0, 0), forceDirection);
-  }
-  let forcePoint = forceDirection.clone().negate();
-  let pos = ball.position;
+
+  const forcePoint = forceDirection.clone().negate();
+  const pos = ball.position;
   forcePoint.vadd(new CANNON.Vec3(pos.x, pos.y, pos.z));
   ball.applyImpulse(forceDirection, forcePoint);
 
   game.update(delta, ball);
 
-  let heading = new THREE.Vector3(forceDirection.x, forceDirection.z, 0);
-  if (heading.length() !== 0) {
+  const heading = new THREE.Vector3(forceDirection.x, forceDirection.z, 0);
+  if (heading.length() !== 0)
     heading.normalize();
-  }
+
   followCamera.update(delta, heading);
 
   renderer.render(SCENE, followCamera);
