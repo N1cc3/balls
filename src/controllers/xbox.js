fc2d496c3b0365e32b7bc1ad7a2a795223040b92
N1cc3 <niclaslindgren91@hotmail.com>
2017-06-11 22:54:42 +0300

    All CRLF changed to LF.

--- src/controllers/xbox.js
+++ src/controllers/xbox.js
@@ -2,34 +2,34 @@
 const XBOX_CONTROLLER_ID = 'Xbox 360 Controller (XInput STANDARD GAMEPAD)';
 
 const buttons = {
-  'a': 0,
-  'b': 1,
-  'x': 2,
-  'y': 3,
-  'lb': 4,
-  'rb': 5,
-  'lt': 6,
-  'rt': 7,
-  'back': 8,
-  'start': 9,
-  'ls': 10,
-  'rs': 11,
-  'up': 12,
-  'down': 13,
-  'left': 14,
-  'right': 15,
+  a: 0,
+  b: 1,
+  x: 2,
+  y: 3,
+  lb: 4,
+  rb: 5,
+  lt: 6,
+  rt: 7,
+  back: 8,
+  start: 9,
+  ls: 10,
+  rs: 11,
+  up: 12,
+  down: 13,
+  left: 14,
+  right: 15,
 };
 
 // The first axis is x, and the second is y.
 // Negative x is left, negative y is up.
 const axes = {
-  'axis_left': [0, 1],
-  'axis_right': [2, 3],
+  axis_left: [0, 1,],
+  axis_right: [2, 3,],
 };
 
 const events = {
-  'button': [],
-  'axis': [],
+  button: [],
+  axis: [],
 };
 
 const callbacks = new Map();
@@ -62,17 +62,17 @@ class XboxController {
     if (this.gamepadIdx === null) return;
     const gamepad = navigator.getGamepads()[this.gamepadIdx];
 
-    Object.entries(buttons).forEach(([button, index]) => {
+    Object.entries(buttons).forEach(([button, index,]) => {
       if (gamepad.buttons[index].pressed) {
         events.button.push(createEvent(button, 1));
       }
     });
 
-    Object.entries(axes).forEach(([axis, indices]) => {
+    Object.entries(axes).forEach(([axis, indices,]) => {
       const axis0 = gamepad.axes[indices[0]];
       const axis1 = gamepad.axes[indices[1]];
       if (Math.abs(axis0) > 0.5 || Math.abs(axis1) > 0.5) {
-        events.axis.push(createEvent(axis, [axis0, axis1]));
+        events.axis.push(createEvent(axis, [axis0, axis1,]));
       }
     });
   }
@@ -87,7 +87,7 @@ class XboxController {
         if (callbacks.has(event.button)) {
           const callback = callbacks.get(event.button);
           callback({
-            'value': event.value,
+            value: event.value,
           });
         }
       });
@@ -105,7 +105,7 @@ class XboxController {
 
 // TODO: Add support for press, hold, and release?
 function createEvent(button, value) {
-  return {'button': button, 'value': value,};
+  return {button: button, value: value,};
 }
 
 export default XboxController;
