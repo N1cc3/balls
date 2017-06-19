0bf0f9a73e28b415e916a882afafec6f28c6c961
N1cc3 <niclaslindgren91@hotmail.com>
2017-06-19 23:14:29 +0300

    Attempt to fix flickering.

--- src/StaticRotationFollowCamera.js
+++ src/StaticRotationFollowCamera.js
@@ -1,23 +1,24 @@
 import * as THREE from 'three';
 import {Utils,} from './Utils';
 
+const POSITION_VELOCITY = 0.1;
+
 class StaticRotationFollowCamera extends THREE.PerspectiveCamera {
 
   constructor(fov = 75, aspect = window.innerWidth / window.innerHeight, near = 0.001, far = 100) {
     super(fov, aspect, near, far);
     this.offset = new THREE.Vector3(0, 1, 1);
-    this.followVelocity = 0.1;
   }
 
   /**
    * Moves camera target towards targetPosition and rotates camera to keep base rotation.
-   * @param  {THREE.Vector3} targetPosition Any object with x, y, and z values work.
+   * @param  {THREE.Vector3} _targetPosition Any object with x, y, and z values work.
    */
   update(_targetPosition) {
     const targetPosition = Utils.toThreeVec(_targetPosition);
     const targetCameraPosition = targetPosition.clone();
     targetCameraPosition.add(this.offset);
-    this.position.lerp(targetCameraPosition, this.followVelocity);
+    this.position.lerp(targetCameraPosition, POSITION_VELOCITY);
     this.lookAt(targetPosition);
   }
 
